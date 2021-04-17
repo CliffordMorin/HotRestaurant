75e5454b4013b32cb777b98a09843f376b0d2939
Clifford Morin <cemorin21@gmail.com>
2021-04-17 14:10:33 -0400

    fixed fetches

--- server.js
+++ server.js
@@ -32,8 +32,6 @@ app.get('/api/tables',(req,res)=> res.json(tableInfo));
 
 app.post('/api/tables',(req,res) =>{
     const newTable = req.body;
-    tableInfo.push(newTable);
-    res.json(newTable);
 
     if (tableInfo.length < 5){
     tableInfo.push(newTable);
